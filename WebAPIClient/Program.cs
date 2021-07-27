008e1385bb1402c698e7b1fdcb9b3da1bb8fe71f
Suyash Joshi <suyashjoshi@users.noreply.github.com>
2021-07-27 16:23:48 -0700

    Updated POST Request body as per API Change

--- WebAPIClient/Program.cs
+++ WebAPIClient/Program.cs
@@ -12,89 +12,97 @@ namespace WebAPIClient {
       request.AddHeader("Content-Type", "application/json");
       request.AddHeader("Accept", "application/json");
       request.AddHeader("Authorization", "Your Bearer Token goes here");
-      var body = @"{" + "\n" +
-                 @"  ""grouping"": {" + "\n" +
-                 @"    ""groupBy"": ""CompanyNumbers""," + "\n" +
-                 @"    ""identifiers"": [" + "\n" +
-                 @"      ""string""" + "\n" +
-                 @"    ]" + "\n" +
-                 @"  }," + "\n" +
-                 @"  ""timeRange"": {" + "\n" +
-                 @"    ""timeFrom"": ""2021-05-01T08:01:21.453Z""," + "\n" +
-                 @"    ""timeTo"": ""2021-05-20T08:01:21.453Z""" + "\n" +
-                 @"  }," + "\n" +
-                 @"  ""additionalFilters"": {" + "\n" +
-                 @"    ""direction"": ""Inbound""," + "\n" +
-                 @"    ""origin"": ""Internal""," + "\n" +
-                 @"    ""callResponse"": ""Answered""," + "\n" +
-                 @"    ""callResponseType"": [" + "\n" +
-                 @"      ""InboundDirect""" + "\n" +
-                 @"    ]," + "\n" +
-                 @"    ""callResult"": {" + "\n" +
-                 @"      ""isCompleted"": true," + "\n" +
-                 @"      ""isAbandoned"": true," + "\n" +
-                 @"      ""isVoiceMail"": true," + "\n" +
-                 @"      ""isConnected"": true" + "\n" +
-                 @"    }," + "\n" +
-                 @"    ""companyHours"": ""BusinessHours""," + "\n" +
-                 @"    ""callDuration"": {" + "\n" +
-                 @"      ""minValueSeconds"": 0," + "\n" +
-                 @"      ""maxValueSeconds"": 0" + "\n" +
-                 @"    }," + "\n" +
-                 @"    ""timeSpent"": {" + "\n" +
-                 @"      ""minValueSeconds"": 0," + "\n" +
-                 @"      ""maxValueSeconds"": 0" + "\n" +
-                 @"    }" + "\n" +
-                 @"  }," + "\n" +
-                 @"  ""responseOptions"": {" + "\n" +
-                 @"    ""counters"": {" + "\n" +
-                 @"      ""allCalls"": {" + "\n" +
-                 @"        ""aggregationType"": ""Sum""" + "\n" +
-                 @"      }," + "\n" +
-                 @"      ""callsByDirection"": {" + "\n" +
-                 @"        ""aggregationType"": ""Sum""" + "\n" +
-                 @"      }," + "\n" +
-                 @"      ""callsByOrigin"": {" + "\n" +
-                 @"        ""aggregationType"": ""Sum""" + "\n" +
-                 @"      }," + "\n" +
-                 @"      ""callsByResponse"": {" + "\n" +
-                 @"        ""aggregationType"": ""Sum""" + "\n" +
-                 @"      }," + "\n" +
-                 @"      ""callsByResponseType"": {" + "\n" +
-                 @"        ""aggregationType"": ""Sum""" + "\n" +
-                 @"      }," + "\n" +
-                 @"      ""callsByResult"": {" + "\n" +
-                 @"        ""aggregationType"": ""Sum""" + "\n" +
-                 @"      }," + "\n" +
-                 @"      ""callsByActions"": {" + "\n" +
-                 @"        ""aggregationType"": ""Sum""" + "\n" +
-                 @"      }" + "\n" +
-                 @"    }," + "\n" +
-                 @"    ""timers"": {" + "\n" +
-                 @"      ""totalCallLength"": {" + "\n" +
-                 @"        ""aggregationType"": ""Sum""" + "\n" +
-                 @"      }," + "\n" +
-                 @"      ""timeSpentByCallSegments"": {" + "\n" +
-                 @"        ""aggregationType"": ""Sum""" + "\n" +
-                 @"      }," + "\n" +
-                 @"      ""callLengthByDirection"": {" + "\n" +
-                 @"        ""aggregationType"": ""Sum""" + "\n" +
-                 @"      }," + "\n" +
-                 @"      ""callLengthByOrigin"": {" + "\n" +
-                 @"        ""aggregationType"": ""Sum""" + "\n" +
-                 @"      }," + "\n" +
-                 @"      ""callLengthByResponse"": {" + "\n" +
-                 @"        ""aggregationType"": ""Sum""" + "\n" +
-                 @"      }," + "\n" +
-                 @"      ""callLengthByResponseType"": {" + "\n" +
-                 @"        ""aggregationType"": ""Sum""" + "\n" +
-                 @"      }," + "\n" +
-                 @"      ""callLengthByResult"": {" + "\n" +
-                 @"        ""aggregationType"": ""Sum""" + "\n" +
-                 @"      }" + "\n" +
-                 @"    }" + "\n" +
-                 @"  }" + "\n" +
-                 @"}";
+      var body =  @"{" + "\n" +
+                  @"  ""grouping"": {" + "\n" +
+                  @"    ""groupBy"": ""CompanyNumbers""" + "\n" +
+                  @"  }," + "\n" +
+                  @"  ""timeRange"": {" + "\n" +
+                  @"    ""timeFrom"": ""2021-07-23T19:18:50.450Z""," + "\n" +
+                  @"    ""timeTo"": ""2021-07-23T19:18:50.450Z""" + "\n" +
+                  @"  }," + "\n" +
+                  @"  ""additionalFilters"": {" + "\n" +
+                  @"    ""direction"": ""Inbound""," + "\n" +
+                  @"    ""origin"": ""Internal""," + "\n" +
+                  @"    ""callResponse"": ""Answered""," + "\n" +
+                  @"    ""callResponseType"": [" + "\n" +
+                  @"      ""InboundDirect""" + "\n" +
+                  @"    ]," + "\n" +
+                  @"    ""callResult"": [" + "\n" +
+                  @"      ""Completed""" + "\n" +
+                  @"    ]," + "\n" +
+                  @"    ""callSegments"": [" + "\n" +
+                  @"      {" + "\n" +
+                  @"        ""callSegment"": ""Ringing""" + "\n" +
+                  @"      }" + "\n" +
+                  @"    ]," + "\n" +
+                  @"    ""companyHours"": ""BusinessHours""," + "\n" +
+                  @"    ""callDuration"": {" + "\n" +
+                  @"      ""minValueSeconds"": 0," + "\n" +
+                  @"      ""maxValueSeconds"": 0" + "\n" +
+                  @"    }," + "\n" +
+                  @"    ""timeSpent"": {" + "\n" +
+                  @"      ""minValueSeconds"": 0," + "\n" +
+                  @"      ""maxValueSeconds"": 0" + "\n" +
+                  @"    }" + "\n" +
+                  @"  }," + "\n" +
+                  @"  ""responseOptions"": {" + "\n" +
+                  @"    ""counters"": {" + "\n" +
+                  @"      ""allCalls"": {" + "\n" +
+                  @"        ""aggregationType"": ""Sum""" + "\n" +
+                  @"      }," + "\n" +
+                  @"      ""callsByDirection"": {" + "\n" +
+                  @"        ""aggregationType"": ""Sum""" + "\n" +
+                  @"      }," + "\n" +
+                  @"      ""callsByOrigin"": {" + "\n" +
+                  @"        ""aggregationType"": ""Sum""" + "\n" +
+                  @"      }," + "\n" +
+                  @"      ""callsByResponse"": {" + "\n" +
+                  @"        ""aggregationType"": ""Sum""" + "\n" +
+                  @"      }," + "\n" +
+                  @"      ""callsByResponseType"": {" + "\n" +
+                  @"        ""aggregationType"": ""Sum""" + "\n" +
+                  @"      }," + "\n" +
+                  @"      ""callsBySegment"": {" + "\n" +
+                  @"        ""aggregationType"": ""Sum""" + "\n" +
+                  @"      }," + "\n" +
+                  @"      ""callsByResult"": {" + "\n" +
+                  @"        ""aggregationType"": ""Sum""" + "\n" +
+                  @"      }," + "\n" +
+                  @"      ""callsByActions"": {" + "\n" +
+                  @"        ""aggregationType"": ""Sum""" + "\n" +
+                  @"      }," + "\n" +
+                  @"      ""callsByCompanyHours"": {" + "\n" +
+                  @"        ""aggregationType"": ""Sum""" + "\n" +
+                  @"      }" + "\n" +
+                  @"    }," + "\n" +
+                  @"    ""timers"": {" + "\n" +
+                  @"      ""totalCallLength"": {" + "\n" +
+                  @"        ""aggregationType"": ""Sum""" + "\n" +
+                  @"      }," + "\n" +
+                  @"      ""timeSpentByCallSegments"": {" + "\n" +
+                  @"        ""aggregationType"": ""Sum""" + "\n" +
+                  @"      }," + "\n" +
+                  @"      ""callLengthByDirection"": {" + "\n" +
+                  @"        ""aggregationType"": ""Sum""" + "\n" +
+                  @"      }," + "\n" +
+                  @"      ""callLengthByOrigin"": {" + "\n" +
+                  @"        ""aggregationType"": ""Sum""" + "\n" +
+                  @"      }," + "\n" +
+                  @"      ""callLengthByResponse"": {" + "\n" +
+                  @"        ""aggregationType"": ""Sum""" + "\n" +
+                  @"      }," + "\n" +
+                  @"      ""callLengthByResponseType"": {" + "\n" +
+                  @"        ""aggregationType"": ""Sum""" + "\n" +
+                  @"      }," + "\n" +
+                  @"      ""callLengthByResult"": {" + "\n" +
+                  @"        ""aggregationType"": ""Sum""" + "\n" +
+                  @"      }," + "\n" +
+                  @"      ""callsLengthByCompanyHours"": {" + "\n" +
+                  @"        ""aggregationType"": ""Sum""" + "\n" +
+                  @"      }" + "\n" +
+                  @"    }" + "\n" +
+                  @"  }" + "\n" +
+                  @"}";
         
       request.AddParameter("application/json", body,  ParameterType.RequestBody);
       IRestResponse response = client.Execute(request);
